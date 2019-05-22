eadb3bdb26cc4d77e59fb1fa2cd60e72313ae92a
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2019-05-22 11:35:24 +0100

    Adds Babel, create Hello World App

---
+++ js/app.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => 
+  <p>Hello World</p>;
+
+ReactDOM.render(<HelloWorld/>, document.getElementById("container"));
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/app.jsx",
+  output: {
+    path: path.resolve(__dirname, "static/js"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
