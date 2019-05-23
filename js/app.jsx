4f5931986c295f965d27c77c57428c6423e786f6
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2019-05-23 10:59:49 +0100

    [all] Implements basic facebook login

--- js/app.jsx
+++ js/app.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const HelloWorld = () => 
-  <p>Hello World</p>;
+const Login = () => {
+  return (
+    <div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true" data-onlogin="login()"></div>
+  );
+};
 
-ReactDOM.render(<HelloWorld/>, document.getElementById("container"));
+const Home = () => {
+  return (
+    <p>You're logged in</p>
+  );
+};
+
+window.ready = () => {
+  FB.getLoginStatus(function(response) {
+    if (response.status === "not_authorized") {
+      ReactDOM.render(<Login/>, document.getElementById("container"));
+      FB.XFBML.parse();
+    } else {
+      ReactDOM.render(<Home/>, document.getElementById("container"));
+    }
+  });
+};
+
+window.login = () => {
+  ReactDOM.render(<Home/>, document.getElementById("container"));
+};
